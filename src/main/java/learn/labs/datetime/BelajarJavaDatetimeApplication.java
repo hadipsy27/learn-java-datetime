22faf20af9b6c6d744bb381de002e7d3c01e7bcb
hadipsy27 <hadipsy27@gmail.com>
2023-08-29 14:42:21 +0700

    First commit

---
+++ src/main/java/learn/labs/datetime/BelajarJavaDatetimeApplication.java
@@ -0,0 +1,14 @@
+package learn.labs.datetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaDatetimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaDatetimeApplication.class, args);
+		System.out.println("Application was Running!!!");
+	}
+
+}
---
+++ src/test/java/learn/labs/datetime/BelajarJavaDatetimeApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.labs.datetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaDatetimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
